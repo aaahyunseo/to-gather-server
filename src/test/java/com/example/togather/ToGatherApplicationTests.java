287b50720d1750a92591934d8eae0d86c72c33e4
aaahyunseo <hs030608@naver.com>
2024-10-05 18:10:05 +0900

    test file

--- src/test/java/com/example/togather/ToGatherApplicationTests.java
+++ src/test/java/com/example/togather/ToGatherApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.togather;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootTest
+@EnableJpaAuditing
 class ToGatherApplicationTests {
 
     @Test
